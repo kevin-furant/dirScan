508b07b3d167f409e6647dd526a3477f23ba7556
fuxiangke <252300663@qq.com>
2026-06-05 09:52:36 +0800

    增加arrow文件统计及dup文件列表

--- script/dirScanResultStat.pl
+++ script/dirScanResultStat.pl
@@ -6,6 +6,7 @@ use File::Spec;
 my $vcfPattern = qr/\.vcf$|vcf.gz$/;
 my $bamPattern = qr/\.bam$/;
 my $fqPattern = qr/fastq\.gz$|fq\.gz$|fq$|fastq$/;
+my $arrowPattern = qr/\.arrow$/;
 
 sub helpInfo() {
 	print "Usage: \n";
@@ -45,7 +46,7 @@ sub report() {
 	my ($user, $reportMD, $reportTsv, $totalSize,
 	$fqTotalSize, $fqDurationSize, $vcfTotalSize,
 	$vcfDurationSize, $bamTotalSize,
-	$bamDurationSize) = @_;
+	$bamDurationSize, $arrowTotalSize, $arrowDurationSize) = @_;
 	my $hrTotalSize = &humanReadable($totalSize);
 	my $hrFqTotalSize = &humanReadable($fqTotalSize);
 	my $hrFqDurationSize = &humanReadable($fqDurationSize);
@@ -53,15 +54,17 @@ sub report() {
 	my $hrVcfDurationSize = &humanReadable($vcfDurationSize);
 	my $hrBamTotalSize = &humanReadable($bamTotalSize);
 	my $hrBamDurationSize = &humanReadable($bamDurationSize);
+	my $hrArrowTotalSize = &humanReadable($arrowTotalSize);
+	my $hrArrowDurationSize = &humanReadable($arrowDurationSize);
 	open RPT, "> $reportMD" or die $!;
 	open TSV, "> $reportTsv" or die $!;
 	print RPT <<"EOF";
-| **User** | **TotalSize** | **VCFTotalSize**  | **VCFDurationSize** | **FQTotalSize** | **FQDurationSize** | **BAMTotalSize** | **BAMDurationSize** |
-| --- | --- | --- | --- | --- | --- | --- | --- |
-| $user | $hrTotalSize | $hrVcfTotalSize | $hrVcfDurationSize | $hrFqTotalSize | $hrFqDurationSize | $hrBamTotalSize | $hrBamDurationSize |	
+| **User** | **TotalSize** | **VCFTotalSize**  | **VCFDurationSize** | **FQTotalSize** | **FQDurationSize** | **BAMTotalSize** | **BAMDurationSize** | **ArrowTotalSize** | **ArrowDurationSize** |
+| --- | --- | --- | --- | --- | --- | --- | --- | --- | --- |
+| $user | $hrTotalSize | $hrVcfTotalSize | $hrVcfDurationSize | $hrFqTotalSize | $hrFqDurationSize | $hrBamTotalSize | $hrBamDurationSize | $hrArrowTotalSize | $hrArrowDurationSize |
 EOF
-	print TSV "User\tTotalSize\tVCFTotalSize\tVCFDurationSize\tFQTotalSize\tFQDurationSize\tBAMTotalSize\tBAMDurationSize\n";
-	printf TSV "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n", $user, $hrTotalSize, $hrVcfTotalSize, $hrVcfDurationSize, $hrFqTotalSize, $hrFqDurationSize, $hrBamTotalSize, $hrBamDurationSize;
+	print TSV "User\tTotalSize\tVCFTotalSize\tVCFDurationSize\tFQTotalSize\tFQDurationSize\tBAMTotalSize\tBAMDurationSize\tArrowTotalSize\tArrowDurationSize\n";
+	printf TSV "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n", $user, $hrTotalSize, $hrVcfTotalSize, $hrVcfDurationSize, $hrFqTotalSize, $hrFqDurationSize, $hrBamTotalSize, $hrBamDurationSize, $hrArrowTotalSize, $hrArrowDurationSize;
 	close RPT; close TSV;	
 }
 sub main() {
@@ -71,16 +74,21 @@ sub main() {
 	my ($user, $dsRet, $durationThreshold) = @ARGV;
 	open IN, "< $dsRet" or die $!;
 	my $totalSize = 0;
-	my ($vcfTotalSize, $vcfDurationSize, $fqTotalSize, $fqDurationSize, $bamTotalSize, $bamDurationSize )= (0) x 6;
+	my ($vcfTotalSize, $vcfDurationSize, $fqTotalSize, $fqDurationSize, $bamTotalSize, $bamDurationSize, $arrowTotalSize, $arrowDurationSize )= (0) x 8;
 	my $inputParentDir = dirname($dsRet);
 	my $vcfFilterTsv = File::Spec->catfile($inputParentDir, "vcf.tsv");
 	my $fqFilterTsv = File::Spec->catfile($inputParentDir, "fastq.tsv");
 	my $bamFilterTsv = File::Spec->catfile($inputParentDir, "bam.tsv");
+	my $arrowFilterTsv = File::Spec->catfile($inputParentDir, "arrow.tsv");
 	my $reportMD = File::Spec->catfile($inputParentDir, "report.md");
 	my $reportTsv = File::Spec->catfile($inputParentDir, "report.tsv");
+	my $dupListTsv = File::Spec->catfile($inputParentDir, "dup.list.tsv");
 	open VCF, "> $vcfFilterTsv" or die $!;
 	open FQ, "> $fqFilterTsv" or die $!;
 	open BAM, "> $bamFilterTsv" or die $!;
+	open ARROW, "> $arrowFilterTsv" or die $!;
+	open DUP, "> $dupListTsv" or die $!;
+	my %dupHash;
 	while(<IN>){
 		chomp;
 		my @cols = split/\t/, $_;
@@ -89,6 +97,8 @@ sub main() {
 		$totalSize += $size;
 		my $eachFile;
 		($eachFile = $cols[0]) =~ s/"//g; 
+		my $fileName = basename($eachFile);
+		push @{$dupHash{$fileName}{$size}}, $eachFile;
 		if ($eachFile =~ $vcfPattern) {
 			print VCF $_,"\n";
 			$vcfTotalSize += $size;
@@ -107,10 +117,26 @@ sub main() {
 			if ($duration >= $durationThreshold) {
 				$fqDurationSize += $size;
 			}
+		}elsif ($eachFile =~ $arrowPattern) {
+			print ARROW $_,"\n";
+			$arrowTotalSize += $size;
+			if ($duration >= $durationThreshold) {
+				$arrowDurationSize += $size;
+			}
 		}
 	}
-	close IN; close VCF; close FQ; close BAM;
-	&report($user, $reportMD, $reportTsv, $totalSize, $fqTotalSize, $fqDurationSize, $vcfTotalSize, $vcfDurationSize, $bamTotalSize, $bamDurationSize);
+	close IN; close VCF; close FQ; close BAM; close ARROW;
+	&report($user, $reportMD, $reportTsv, $totalSize, $fqTotalSize, $fqDurationSize, $vcfTotalSize, $vcfDurationSize, $bamTotalSize, $bamDurationSize, $arrowTotalSize, $arrowDurationSize);
+	print DUP "DupSize\tSize(bytes)\tFilePathList\n";
+	while (my ($fn, $sizeHashPtr) = each %dupHash) {
+		while (my ($size, $fileListPtr) = each %$sizeHashPtr) {
+			next if (@$fileListPtr <= 1);
+			my $dupNum = scalar(@$fileListPtr);
+			my $fileListStr = join(",", @$fileListPtr);
+			print DUP "$dupNum\t$size\t$fileListStr\n";
+		}
+	}
+	close DUP;
 }
 
 &main();
